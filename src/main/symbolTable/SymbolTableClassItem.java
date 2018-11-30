7cc5b9e1b788bb27741e19df96e3b394dcecd331
Farzad Habibi <farzadhabibii98@gmail.com>
2018-11-30 12:34:42 +0330

    grand parents method over riding handled

--- src/main/symbolTable/SymbolTableClassItem.java
+++ src/main/symbolTable/SymbolTableClassItem.java
@@ -1,8 +1,10 @@
 package main.symbolTable;
 
 public class SymbolTableClassItem extends  SymbolTableItem {
+
     public  SymbolTableClassItem(String name) {
         this.name = name;
+
     }
     @Override
     public String getKey() {
